87a0032054d798d19c20c5f4d6c6871d579cc29a
gulzhaan <abdykapparg@gmail.com>
2024-05-04 08:19:35 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import RegistrationForm from './RegistrationForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Register</h1>
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
+
